c80e36d54925c7c5cf438cb5c572bf7060a78165
Shaotong Li <lstong@caen-vnc-mi17.engin.umich.edu>
2024-02-15 16:26:57 -0500

    fixed decoder

--- decoder.sv
+++ decoder.sv
@@ -41,33 +41,13 @@ assign Weights_boundary =current_Weights_boundary;
 assign Req =nx_Req;
 always_ff @( posedge clk ) begin 
     if(reset)begin
-<<<<<<< HEAD
-        state <= 'd0;
-        current_Weights_boundary <= 'd0;
-        current_replay_Iter <= 'd0;
-        current_packet <= 'd0;
-        current_Num_FV <= 'd0; 
-        current_Req<= 'd0;
-
-        current_replay_iter_flag<= 'd0;
-    end
-    else begin
-        state <= nx_state;
-        current_Weights_boundary <= nx_Weights_boundary;
-        current_replay_Iter <= nx_replay_Iter;
-        current_packet <= nx_packet;
-        current_Num_FV <= nx_Num_FV; 
-        current_Req<= nx_Req;
-
-        current_replay_iter_flag<=replay_iter_flag;
-=======
         state <= #1 'd0;
         current_Weights_boundary <= #1 'd0;
         current_replay_Iter <= #1 'd0;
         current_packet <= #1 'd0;
         current_Num_FV <= #1 'd0; 
         current_Req<= #1 'd0;
-        fifo_stall<= #1 'd0;
+    
         current_replay_iter_flag<= #1 'd0;
     end
     else begin
@@ -77,9 +57,8 @@ always_ff @( posedge clk ) begin
         current_packet <= #1 nx_packet;
         current_Num_FV <= #1 nx_Num_FV; 
         current_Req<= #1 nx_Req;
-        fifo_stall<= #1 nx_fifo_stall;
+        
         current_replay_iter_flag<= #1 replay_iter_flag;
->>>>>>> b287e6ae062218583a857c3b2a6cd59bb3923ee8
     end
 end
 always_comb begin
