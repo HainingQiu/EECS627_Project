01d0dbad68dbb0c7d90513725ba9fa97ae3d3966
Haining Qiu <hqiu37@wisc.edu>
2024-02-19 02:42:08 -0500

    Top_tb mem dump 2 file
    But it's not tested

--- Top_tb.sv
+++ Top_tb.sv
@@ -3,33 +3,96 @@ module Top_tb();
 
 logic clk, reset;
 logic task_complete;
+integer file1, file2, file3, file4;
 
 Top iTop_DUT(
 .*
 );
 
+///// Clock Gen /////
 always #5 clk = ~clk;
+
+///// Replay Iteration FF /////
+/* logic [1:0] replay_Iter_ff;
+always @(posedge clk) begin
+	if (reset)
+		replay_Iter_ff <= '0;
+	else
+		replay_Iter_ff <= iTop_DUT.Current_replay_Iter;
+end */
+///// Dump Mem /////
+always @(iTop_DUT.Current_replay_Iter, task_complete) begin
+	if (iTop_DUT.Current_replay_Iter === 2'b01 & !task_complete)
+		dumpPongBuf2file(file1);
+	if (iTop_DUT.Current_replay_Iter === 2'b10 & !task_complete)
+		dumpPongBuf2file(file2);
+	if (iTop_DUT.Current_replay_Iter === 2'b11 & !task_complete)
+		dumpPongBuf2file(file3);
+	if (task_complete)
+		dumpPongBuf2file(file4);
+end
+/* always @(posedge clk) begin
+    if ((replay_Iter_ff == 2'b00 && iTop_DUT.Current_replay_Iter == 2'b01)) begin
+		dumpPongBuf2file(file1);
+    end
+	if ((replay_Iter_ff == 2'b01 && iTop_DUT.Current_replay_Iter == 2'b10)) begin
+		dumpPongBuf2file(file2);
+    end
+	if ((replay_Iter_ff == 2'b10 && iTop_DUT.Current_replay_Iter == 2'b11)) begin
+		dumpPongBuf2file(file3);
+    end
+	if (task_complete) begin
+		dumpPongBuf2file(file4);
+    end
+end */
 
 initial begin
 
 init();
-//forever begin
-//	if(task_complete)begin
-//		@(posedge clk);
-		
-//	end
-//	@(posedge clk);
-//end
+
 repeat (5000) @(negedge clk);
+
+///// File Close /////
+$fclose(file1);
+$fclose(file2);
+$fclose(file3);
+$fclose(file4);
+$display("Sim completed. Check files..");
+
 $finish; 
+
 end
-//
+
+
 task automatic init();
+
+///// Output File /////
+file1 = $fopen("Iter0.txt", "w");
+if (!file1) begin
+	$display("Error opening Iter0.txt.");
+	$stop;
+end
+file2 = $fopen("Iter1.txt", "w");
+if (!file2) begin
+	$display("Error opening Iter1.txt.");
+	$stop;
+end
+file3 = $fopen("Iter2.txt", "w");
+if (!file3) begin
+	$display("Error opening Iter2.txt.");
+	$stop;
+end
+file4 = $fopen("Iter3.txt", "w");
+if (!file4) begin
+	$display("Error opening Iter3.txt.");
+	$stop;
+end
 
 clk = 0;
 reset = 1; // avtive high sync reset
 @(negedge clk);
 reset = 0; // go
+
 ///// FV SRAM Initialization /////
 $readmemb("./data/feature_value_bank0.txt",
 		  iTop_DUT.Big_FV_wrapper_0_U.ping_buffer[0].BIG_FV_SRAM_u.mem);
@@ -70,11 +133,19 @@ $readmemb("./data/nb_bank2.txt",
 $readmemb("./data/nb_bank3.txt",
 		  iTop_DUT.S_Neighbor_SRAM_integration_U.
 		  SRAM_Instantiations[3].Neighbor_SRAM_U.mem);
+endtask
 
-
-
-
-
+// Dump all contents in 4 BIG_FV_SRAMs into a single file
+// This will be called 4 times for 4 replay iterations
+// NOTE that they dump all 4 SRAM contents into one file at a times
+// so please access the file in order
+task automatic dumpPongBuf2file (integer filex);
+	for (int i = 0; i < `Num_Banks_all_FV; i++) begin
+		for (int j = 0; j < 1024; j++) begin
+			$fwrite(filex, "%b\n",
+			iTop_DUT.Big_FV_wrapper_1_U.ping_buffer[i].BIG_FV_SRAM_u.mem[j]);
+		end
+	end
 endtask
 
 
