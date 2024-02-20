04b5521629f69b2f02c266c03f3a1f70c930b7af
Shengwei Wang <shengwsw@umich.edu>
2024-02-20 12:37:45 -0500

    add neighbour info sram

--- updated_Project/Neighbor_Info_Integration.sv
+++ updated_Project/Neighbor_Info_Integration.sv
@@ -45,7 +45,7 @@ Neighbor_Info_CNTL Neighbor_Info_CNTL_U(
 generate
     genvar i;
     for(i=0;i<`num_bank_neighbor_info;i=i+1)begin:Nb_Info_SRAM_init
-        Neighbor_Info_SRAM Neighbor_Info_SRAM_U(
+        Nb_Info_SRAM_64 Neighbor_Info_SRAM_U(
             .Q(Data_SRAM_in[i]),
             .CLK(clk),
             .CEN(Neighbor_info_CNTL2SRAM_interface_out[i].CEN),
