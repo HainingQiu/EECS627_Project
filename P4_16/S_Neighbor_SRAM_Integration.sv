99a0a1e8b31ea5d0c66b4f205ccdf81febe0099b
Zhao Zhang <zzyyds@caen-vnc-mi18.engin.umich.edu>
2024-03-23 18:32:39 -0400

    S_Neighbor

--- P4_16/S_Neighbor_SRAM_Integration.sv
+++ P4_16/S_Neighbor_SRAM_Integration.sv
@@ -3,11 +3,56 @@ module S_Neighbor_SRAM_integration(
     input clk,
     input reset,
     // input winc,
-	input Neighbor_info2Neighbor_FIFO	wdata,
+	// input Neighbor_info2Neighbor_FIFO	wdata,
+    input 	wdata_valid,
+	input 	[`Neighbor_info_bandwidth-1:0]wdata_addr,
+    input 	[$clog2(`Num_Edge_PE)-1:0]wdata_PE_tag,
 
-    output NeighborID_SRAM2Edge_PE[`Num_Edge_PE-1:0] NeighborID_SRAM2Edge_PE_out,
+    // output NeighborID_SRAM2Edge_PE[`Num_Edge_PE-1:0] NeighborID_SRAM2Edge_PE_out,
+    output logic NeighborID_SRAM2Edge_PE_out_sos_0, // start of streaming
+    output logic NeighborID_SRAM2Edge_PE_out_eos_0,//  end of streaming
+    output logic[$clog2(`max_degree_Iter)-1:0] NeighborID_SRAM2Edge_PE_out_Neighbor_num_Iter_0,
+    output logic [`Neighbor_ID_bandwidth-1:0] NeighborID_SRAM2Edge_PE_out_Neighbor_id_0,
+	output logic NeighborID_SRAM2Edge_PE_out_sos_1, // start of streaming
+    output logic NeighborID_SRAM2Edge_PE_out_eos_1,//  end of streaming
+    output logic[$clog2(`max_degree_Iter)-1:0] NeighborID_SRAM2Edge_PE_out_Neighbor_num_Iter_1,
+    output logic [`Neighbor_ID_bandwidth-1:0] NeighborID_SRAM2Edge_PE_out_Neighbor_id_1,
+	output logic NeighborID_SRAM2Edge_PE_out_sos_2, // start of streaming
+    output logic NeighborID_SRAM2Edge_PE_out_eos_2,//  end of streaming
+    output logic[$clog2(`max_degree_Iter)-1:0] NeighborID_SRAM2Edge_PE_out_Neighbor_num_Iter_2,
+    output logic [`Neighbor_ID_bandwidth-1:0] NeighborID_SRAM2Edge_PE_out_Neighbor_id_2,
+	output logic NeighborID_SRAM2Edge_PE_out_sos_3, // start of streaming
+    output logic NeighborID_SRAM2Edge_PE_out_eos_3,//  end of streaming
+    output logic[$clog2(`max_degree_Iter)-1:0] NeighborID_SRAM2Edge_PE_out_Neighbor_num_Iter_3,
+    output logic [`Neighbor_ID_bandwidth-1:0] NeighborID_SRAM2Edge_PE_out_Neighbor_id_3,
+
     output logic wfull
 );
+Neighbor_info2Neighbor_FIFO	wdata;
+NeighborID_SRAM2Edge_PE [`Num_Edge_PE-1:0] NeighborID_SRAM2Edge_PE_out;
+assign wdata.valid=wdata_valid;
+assign wdata.addr=wdata_addr;
+assign wdata.PE_tag=wdata_PE_tag;
+
+assign NeighborID_SRAM2Edge_PE_out_sos_0=NeighborID_SRAM2Edge_PE_out[0].sos;
+assign NeighborID_SRAM2Edge_PE_out_eos_0=NeighborID_SRAM2Edge_PE_out[0].eos;
+assign NeighborID_SRAM2Edge_PE_out_Neighbor_num_Iter_0=NeighborID_SRAM2Edge_PE_out[0].Neighbor_num_Iter;
+assign NeighborID_SRAM2Edge_PE_out_Neighbor_id_0=NeighborID_SRAM2Edge_PE_out[0].Neighbor_ids;
+
+assign NeighborID_SRAM2Edge_PE_out_sos_1=NeighborID_SRAM2Edge_PE_out[1].sos;
+assign NeighborID_SRAM2Edge_PE_out_eos_1=NeighborID_SRAM2Edge_PE_out[1].eos;
+assign NeighborID_SRAM2Edge_PE_out_Neighbor_num_Iter_1=NeighborID_SRAM2Edge_PE_out[1].Neighbor_num_Iter;
+assign NeighborID_SRAM2Edge_PE_out_Neighbor_id_1=NeighborID_SRAM2Edge_PE_out[1].Neighbor_ids;
+
+assign NeighborID_SRAM2Edge_PE_out_sos_2=NeighborID_SRAM2Edge_PE_out[2].sos;
+assign NeighborID_SRAM2Edge_PE_out_eos_2=NeighborID_SRAM2Edge_PE_out[2].eos;
+assign NeighborID_SRAM2Edge_PE_out_Neighbor_num_Iter_2=NeighborID_SRAM2Edge_PE_out[2].Neighbor_num_Iter;
+assign NeighborID_SRAM2Edge_PE_out_Neighbor_id_2=NeighborID_SRAM2Edge_PE_out[2].Neighbor_ids;
+
+assign NeighborID_SRAM2Edge_PE_out_sos_3=NeighborID_SRAM2Edge_PE_out[3].sos;
+assign NeighborID_SRAM2Edge_PE_out_eos_3=NeighborID_SRAM2Edge_PE_out[3].eos;
+assign NeighborID_SRAM2Edge_PE_out_Neighbor_num_Iter_3=NeighborID_SRAM2Edge_PE_out[3].Neighbor_num_Iter;
+assign NeighborID_SRAM2Edge_PE_out_Neighbor_id_3=NeighborID_SRAM2Edge_PE_out[3].Neighbor_ids;
 logic rinc_Neighbor_CNTL2FIFO;
 Neighbor_info2Neighbor_FIFO rdata;
 logic empty;
